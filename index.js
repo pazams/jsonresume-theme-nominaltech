744c9a7e9e506811ef0d4b1a9430fe235efce4a2
zeikko <zeikko@zeikko.fi>
2014-08-18 18:54:51 +0300

    Make dates smaller, add month to dates

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function render(resumeObject) {
 
 
 	_.each(resumeObject.work, function(w){
-		w.startDateYear = w.startDate.substr(0,4);
+		w.startDateYear = w.startDate.substr(0,7);
 		if(w.endDate) {
-			w.endDateYear = w.endDate.substr(0,4);
+			w.endDateYear = w.endDate.substr(0,7);
 		} else { 
 			w.endDateYear = 'Present'
 		}

646e605edd11706c94bb900c2407425aae52db81
Josh Glendenning <joshglendenning@gmail.com>
2014-11-05 01:26:17 -0600

    Updated style

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ function render(resumeObject) {
 		}
 	});
 	_.each(resumeObject.education, function(e){
-    if( !e.area || !e.studyType ){
-      e.educationDetail = (e.area == null ? '' : e.area) + (e.studyType == null ? '' : e.studyType);
-    }  else {
-      e.educationDetail = e.area + ", "+ e.studyType;
-    }
+		if( !e.area || !e.studyType ){
+			e.educationDetail = (e.area == null ? '' : e.area) + (e.studyType == null ? '' : e.studyType);
+		}  else {
+			e.educationDetail = e.area + ", "+ e.studyType;
+		}
 		e.startDateYear = e.startDate.substr(0,4);
 		if(e.endDate) {
 			e.endDateYear = e.endDate.substr(0,4);
@@ -37,17 +37,17 @@ function render(resumeObject) {
 	});
 	if(resumeObject.basics && resumeObject.basics.email) {
 		resumeObject.basics.gravatar = gravatar.url(resumeObject.basics.email, {
-                        s: '100',
-                        r: 'pg',
-                        d: 'mm'
-                    });
+			'size': '100',
+			'rating': 'g',
+			'default': 'blank'
+		});
 	}
 
 	var theme = fs.readFileSync(__dirname + '/resume.template', 'utf8');
 	var css = fs.readFileSync(__dirname + "/resume.css", "utf-8");
 	resumeObject.css = css;
 	var resumeHTML = Mustache.render(theme, resumeObject);
-	
+
 
 	return resumeHTML;
 };
